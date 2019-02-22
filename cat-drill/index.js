8c916bc2171386368e6530dfeab047d9e7553881
Mehul Patel <mehulpatel118@gmail.com>
2019-02-22 16:42:33 -0500

    updated cat

--- cat-drill/index.js
+++ cat-drill/index.js
@@ -2,14 +2,15 @@
 /* global $ */
 function changeImage(){
   $('.thumbnail').on('click', function(event) {
+    
+   
+
+
+    $('.hero').find('img').attr('src',$(this).find('img').attr('src'));
+    $('.hero').find('img').attr('alt',$(this).find('img').attr('alt'));
 
     
-    $('.hero').find('img').attr('src',$(this).find('img').attr('src'));
-
-    // $('.hero').children('src',$(this).find('img').attr('src'));
-   
   });
 }
-
 
 $(changeImage);
